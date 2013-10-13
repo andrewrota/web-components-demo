421c460eebd8da2714bf54736355172b26843604
Andrew Rota <rota.andrew@gmail.com>
2013-10-12 23:05:06 -0400

    Going old school with getElementById

--- shadowdom.js
+++ shadowdom.js
@@ -1,6 +1,6 @@
 (function() {
     'use strict';
-    var host = document.querySelector('#host');
+    var host = document.getElementById('host');
     var shadow = host.webkitCreateShadowRoot();
     shadow.innerHTML += '<style>h1,h3 { margin: 0; color: blue; text-align: center;}</style>';
     shadow.innerHTML += '<h3>My name is </h3><h1><content select="h1"></content></h1>';
