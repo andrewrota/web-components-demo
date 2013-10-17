dcaa996cb03a4690058cc7a46eced8107571605c
Andrew Rota <rota.andrew@gmail.com>
2013-10-16 21:16:11 -0400

    Add demonstration of element.webkitShadowRoot

--- shadowdom.js
+++ shadowdom.js
@@ -6,3 +6,8 @@
     shadow.innerHTML += '<h3>My name is </h3><h1><content select="h1"></content></h1>';
     shadow.innerHTML += '<p>This is the shadow DOM</p>';
 }());
+(function() {
+  var shadow = document.getElementById('host').webkitShadowRoot;
+  var shadowParagraph = shadow.querySelector('p');
+  shadowParagraph.innerHTML += '...and JS can add to this element from the outside document using <code>webkitShadowRoot</code>';
+}());
